c2128c553606fa9c68c46cf8d3bea1d000e95402
MCaetan0 <marceloc.lima349@gmail.com>
2025-06-16 18:07:00 -0300

    fix: arruma tela inicial

--- src/screens/on-boarding/index.tsx
+++ src/screens/on-boarding/index.tsx
@@ -1,10 +1,11 @@
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
 
 import { NavigationProp } from "@react-navigation/native";
 import { Image } from "react-native";
 import Button from "../../components/button";
 import Typography from "../../components/text";
 import { styles } from "./on-boarding.style";
+import { colors } from "../../styles/colors";
 
 interface ButtonsProps {
   navigation: NavigationProp<any>;
@@ -13,10 +14,15 @@ interface ButtonsProps {
 const OnboardingScreen = ({ navigation }: ButtonsProps) => {
   return (
     <>
+      <StatusBar translucent backgroundColor="transparent" />
       <View>
         <Image
           source={require("../../../assets/init.png")}
           style={styles.Image}
+        />
+        <Image
+          source={require("../../../assets/default/prazologofinal.png")}
+          style={styles.logo}
         />
         <View style={styles.divContent}>
           <Typography
@@ -38,30 +44,77 @@ const OnboardingScreen = ({ navigation }: ButtonsProps) => {
       </View>
 
       <Buttons navigation={navigation} />
+      <Typography
+        variant="SM"
+        family="medium"
+        style={{
+          textAlign: "center",
+          color: colors.neutral["500"],
+        }}
+      >
+        Não possui conta?
+        <Typography
+          onPress={() => navigation.navigate("Register")}
+          variant="SM"
+          family="bold"
+          style={{
+            marginTop: 16,
+            textAlign: "center",
+            color: colors.brand.default,
+          }}
+        >
+          {" "}
+          Registre-se
+        </Typography>
+      </Typography>
     </>
   );
 };
 
 const Buttons = ({ navigation }: ButtonsProps) => (
   <View style={styles.buttonContainer}>
-    <Button
-      onPress={() => navigation.navigate("Login")}
-      variant="primary"
-      type="fill"
-      size="large"
-      disabled={false}
+    <View style={{ width: "100%", marginTop: 0 }}>
+      <Button
+        onPress={() => navigation.navigate("Login")}
+        variant="primary"
+        type="fill"
+        size="large"
+        disabled={false}
+      >
+        Acessar minha conta
+      </Button>
+    </View>
+    <View
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        marginTop: 2,
+      }}
     >
-      Acessar minha conta
-    </Button>
-    <Button
-      onPress={() => navigation.navigate("Register")}
-      variant="primary"
-      type="outlined"
-      size="large"
-      disabled={false}
-    >
-      Criar uma nova conta
-    </Button>
+      <View style={styles.line} />
+      <Typography
+        variant="SM"
+        family="medium"
+        style={{ color: colors.neutral[500] }}
+      >
+        Ou faça login com
+      </Typography>
+      <View style={styles.line} />
+    </View>
+
+    <View style={{ width: "100%", marginTop: 7 }}>
+      <Button
+        variant="neutral"
+        type="outlined"
+        /*    onPress={Login.googleLogin} */
+      >
+        <Image source={require("../../../assets/google.png")} />
+        Google
+      </Button>
+    </View>
   </View>
 );
 
